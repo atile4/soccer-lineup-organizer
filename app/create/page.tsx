69e190c15a30474ee7f181e9c347d3df94aa17e8
atile4 <atile4444@gmail.com>
2026-05-06 16:56:54 -0700

    add /create/ and /manage/ page routes

---
+++ app/create/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import AppHeader from "../components/AppHeader/AppHeader";
+
+export default function CreatePage() {
+  // TODO: AUTHENTICATION POINT #4 - Add logout functionality
+  const handleLogout = () => {
+    // TODO: Implement logout logic here
+    // Example: signOut() from your auth provider
+    console.log("Logout clicked - Implement your logout logic here");
+    // For now, just redirect to home
+    // router.push('/')
+  };
+
+  return (
+    <div className="h-screen flex flex-col">
+      <AppHeader userName="Guest User" onLogout={handleLogout} />
+
+      <main className="flex-1 flex overflow-hidden py-4 gap-4">
+        {/* Content will go here */}
+      </main>
+    </div>
+  );
+}
