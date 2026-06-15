588fab26650323e8152cf7b77e2b678ee89edb4e
atile4 <atticus1467@gmail.com>
2026-06-14 22:51:02 -0700

    firebase config

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,23 +1,14 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp, getApps } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAssrGIxecgvqntbQKlQkbqLkSt0m9yeEw",
-  authDomain: "soccer-lineup-organizer-w.firebaseapp.com",
-  projectId: "soccer-lineup-organizer-w",
-  storageBucket: "soccer-lineup-organizer-w.firebasestorage.app",
-  messagingSenderId: "859070845612",
-  appId: "1:859070845612:web:a17ce5632c3e3375ebc509",
-  measurementId: "G-D65ZHTZ3SL"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+export const auth = getAuth(app);
