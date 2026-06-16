29d2c9eda680cb09bf8fba7eada2f8cd3c331f01
atile4 <atticus1467@gmail.com>
2026-06-15 18:08:25 -0700

    feat: remove arrow button

--- app/components/AppHeader/ProfileMenu.tsx
+++ app/components/AppHeader/ProfileMenu.tsx
@@ -88,21 +88,6 @@ export default function ProfileMenu({
             user?.displayName && getInitials(user.displayName)
           )}
         </div>
-
-        {/* Chevron */}
-        <svg
-          className={`${buttonStyles.chevron} ${isOpen ? "rotate-180" : ""}`}
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M19 9l-7 7-7-7"
-          />
-        </svg>
       </button>
 
       {/* Popover Menu */}
