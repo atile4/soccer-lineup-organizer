9656b9e5bab71ab08f04cb33d7bd3c679ddd9d22
atile4 <atticus1467@gmail.com>
2026-06-28 21:23:59 -0700

    feat: fetch teams from DB in dropdown

--- services/teams.ts
+++ services/teams.ts
@@ -2,8 +2,7 @@ import { supabase } from "@/lib/supabase";
 
 export async function fetchTeams(userId: string) {
   // @TODO include a user id to fetch
-  const { data, error } = await supabase.from("players").select("*").eq("userId", userId);;
-
+  const { data, error } = await supabase.from("teams").select("*").eq("user_id", userId);
   if (error) throw error;
   return data;
 }
