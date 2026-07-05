fbb424ef0fbe2b80a567e2a3356a3859ef64fb12
atile4 <atticus1467@gmail.com>
2026-07-04 20:30:19 -0700

    remove unused functions/methods/components

--- app/manage/page.tsx
+++ app/manage/page.tsx
@@ -14,7 +14,7 @@ export default function ManagePage() {
 
   return (
     <div className="h-screen flex flex-col">
-      <AppHeader userName="Guest User" onLogout={handleLogout} />
+      <AppHeader />
 
       <main className="flex-1 flex overflow-hidden py-4 gap-4">
         {/* Content will go here */}
