452e8daad487bb135fc6548514c03612479bbfc5
Atticus <144712135+atile4@users.noreply.github.com>
2026-07-04 20:35:33 -0700

    Overall cleanup (#18)
    * remove unused functions/methods/components
    
    * cleanup

--- app/manage/page.tsx
+++ app/manage/page.tsx
@@ -3,18 +3,9 @@
 import AppHeader from "../components/AppHeader/AppHeader";
 
 export default function ManagePage() {
-  // TODO: AUTHENTICATION POINT #4 - Add logout functionality
-  const handleLogout = () => {
-    // TODO: Implement logout logic here
-    // Example: signOut() from your auth provider
-    console.log("Logout clicked - Implement your logout logic here");
-    // For now, just redirect to home
-    // router.push('/')
-  };
-
   return (
     <div className="h-screen flex flex-col">
-      <AppHeader userName="Guest User" onLogout={handleLogout} />
+      <AppHeader />
 
       <main className="flex-1 flex overflow-hidden py-4 gap-4">
         {/* Content will go here */}
