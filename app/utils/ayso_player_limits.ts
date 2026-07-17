ecc1b61666272223ad23c69a6a30245325fd984f
atile4 <atticus1467@gmail.com>
2026-07-16 21:12:02 -0700

    feat: player limit constants

---
+++ app/utils/ayso_player_limits.ts
@@ -0,0 +1,19 @@
+/**
+	@TODO
+	implement warnings for AYSO "everybody plays" rule
+	1. The Golden Rule: No player may play a full 4 quarters unless every 
+			other teammate on the roster has played at least 3 quarters.
+	2. U10: A player can play goalkeeper for a maximum of 2 quarters per game.
+	3. U12: A player can play goalkeeper for a maximum of 3 quarters per game.
+	*/
+
+export const MAX_PLAYERS = 11;
+
+export const MAX_PLAYERS_BY_DIV = {
+  u8: 4,
+  u10: 7,
+  u12: 9,
+  u14: 11,
+  u16: 11,
+  u18: 11,
+};
